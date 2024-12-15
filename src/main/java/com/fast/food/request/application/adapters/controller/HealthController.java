2b91b75889d0ce2765dda9b468e2b8546c51e6b8
samueldesouza <samueldesouza.dev@gmail.com>
2024-12-15 19:57:51 -0300

    Criando HealthController

--- src/main/java/com/fast/food/request/application/adapters/controller/HealthController.java
+++ src/main/java/com/fast/food/request/application/adapters/controller/HealthController.java
@@ -1,15 +1,18 @@
 package com.fast.food.request.application.adapters.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/health")
 public class HealthController {
 
     @GetMapping
     public String healthCheck() {
+        log.info("HealthCheck LIVE");
         return "OK";
     }
 }
