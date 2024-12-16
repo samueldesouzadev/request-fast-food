8745cf53ac987aeeb83238d5080e0d54b0d16fe1
samueldesouza <samueldesouza.dev@gmail.com>
2024-12-16 14:23:26 -0300

    Migrando banco de dados AWS

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,29 +1,21 @@
-INSERT INTO CLIENT(ID_CLIENT, CPF_CLIENT, EMAIL_CLIENT, NAME_CLIENT) VALUES(1, '12312312300', 'carla@email.com', 'Carla');
-INSERT INTO CLIENT(ID_CLIENT, CPF_CLIENT, EMAIL_CLIENT, NAME_CLIENT) VALUES(2, '99999999999', 'maria@email.com', 'Maria');
-INSERT INTO CLIENT(ID_CLIENT, CPF_CLIENT, EMAIL_CLIENT, NAME_CLIENT) VALUES(3, '77777777777', 'guilherme@email.com', 'Guilherme');
+-- Limpa as tabelas CLIENT e PRODUCT
+DELETE FROM CLIENT;
+DELETE FROM PRODUCT;
 
-ALTER SEQUENCE CLIENT_SEQ RESTART WITH 3;
+-- Reseta os IDs (se necessário)
+ALTER TABLE CLIENT AUTO_INCREMENT = 1;
+ALTER TABLE PRODUCT AUTO_INCREMENT = 1;
 
+-- Reinsere os dados
+INSERT INTO CLIENT (CPF_CLIENT, EMAIL_CLIENT, NAME_CLIENT)
+VALUES
+('12312312300', 'carla@email.com', 'Carla'),
+('99999999999', 'maria@email.com', 'Maria'),
+('77777777777', 'guilherme@email.com', 'Guilherme');
 
-INSERT INTO PRODUCT(ID_PRODUCT, NAME_PRODUCT, DESCRIPTION, PRICE, CATEGORY) VALUES(1,
-                                                                                   'Quarterão com Queijo',
-                                                                                   'Um hambúrguer (100% carne bovina), queijo processado sabor cheddar, picles, cebola, ketchup, mostarda e pão com gergelim.',
-                                                                                   7.00,
-                                                                                   'LUNCH');
-INSERT INTO PRODUCT(ID_PRODUCT, NAME_PRODUCT, DESCRIPTION, PRICE, CATEGORY) VALUES(2,
-                                                                                   'McFritas Grande',
-                                                                                   'Deliciosas batatas selecionadas, fritas, crocantes por fora, macias por dentro, douradas, irresistíveis, saborosas, famosas, e todos os outros adjetivos positivos que você quiser dar',
-                                                                                   5.38,
-                                                                                   'ACCOMPANIMENT');
-INSERT INTO PRODUCT(ID_PRODUCT, NAME_PRODUCT, DESCRIPTION, PRICE, CATEGORY) VALUES(3,
-                                                                                   'Coca-Cola 500ml',
-                                                                                   'Bebida gelada na medida certa para matar sua sede. Refrescante Coca-Cola 500ml',
-                                                                                   5.90,
-                                                                                   'DRINK');
-INSERT INTO PRODUCT(ID_PRODUCT, NAME_PRODUCT, DESCRIPTION, PRICE, CATEGORY) VALUES(4,
-                                                                                   'McShake Morango 400ml',
-                                                                                   'O novo McShake é feito com leite e batido na hora com calda de morango',
-                                                                                   8.99,
-                                                                                   'DESSERT');
-
-ALTER SEQUENCE PRODUCT_SEQ RESTART WITH 4;
+INSERT INTO PRODUCT (NAME_PRODUCT, DESCRIPTION, PRICE, CATEGORY)
+VALUES
+('Quarterão com Queijo', 'Um hambúrguer (100% carne bovina), queijo processado sabor cheddar, picles, cebola, ketchup, mostarda e pão com gergelim.', 7.00, 'LUNCH'),
+('McFritas Grande', 'Deliciosas batatas selecionadas, fritas, crocantes por fora, macias por dentro, douradas, irresistíveis, saborosas, famosas, e todos os outros adjetivos positivos que você quiser dar', 5.38, 'ACCOMPANIMENT'),
+('Coca-Cola 500ml', 'Bebida gelada na medida certa para matar sua sede. Refrescante Coca-Cola 500ml', 5.90, 'DRINK'),
+('McShake Morango 400ml', 'O novo McShake é feito com leite e batido na hora com calda de morango', 8.99, 'DESSERT');
